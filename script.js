ca006c0b84d7711625033b6737e1fef5ba173ddd
arnav19999 <73399105+arnav19999@users.noreply.github.com>
2022-10-22 15:55:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function open_curtain()
+{
+    $("#curtain1").animate({width:20},1000);
+    $("#curtain2").animate({width:20},1000);
+}
+function close_curtain()
+{
+    $("#curtain1").animate({width:300},1000);
+    $("#curtain2").animate({width:300},1000);
+}
